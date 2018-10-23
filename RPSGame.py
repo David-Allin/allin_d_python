821f363ce1686692df8e2e8e52771024d80fee7c
Allin <d_allin@tss-hr420-dm21.fc.ca>
2018-10-23 13:35:25 -0400

    Water Logic

--- RPSGame.py
+++ RPSGame.py
@@ -1,7 +1,15 @@
-from random import randint
+print("Rules that govern the state of water")
+current_temp = False
+while current_temp is False:
+	current_temp = input("Enter a temperature:\n")
+	print("you unput:", current_temp)
+	if (int(current_temp) < 0 or (int(current_temp) == 0)):
+		print("Water is a solid! cuzit's at or below freezing")
+		current_temp = False
 
-choices = ["Rock", "Paper", "Scissors"]
-
-computer_choice = choices[randint(0,2)]
-
-print("computer chooses: ", computer_choice)
+	elif (int(current_temp) < 100):
+		print("water is a liquid! because it's above preezing and below boiling")
+		current_temp = False
+	elif (int(current_temp) > 100 or (int(current_temp) == 100)):
+		print("Water is a gas! because it is.. extremly hot")
+		current_temp = False
